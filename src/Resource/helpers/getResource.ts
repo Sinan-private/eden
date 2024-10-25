d1ee11a15c8dd9697919274d5a6f702612692bd0
Sinan Theuvsen <sinan.alk@gmail.com>
2024-10-25 21:42:42 +0200

    Good enough.
    I solved the issue for now and did the necessary documentation.

--- src/Resource/helpers/getResource.ts
+++ src/Resource/helpers/getResource.ts
@@ -1,8 +1,17 @@
 import {ResourceState} from "../genericTypes.ts";
 import {Resource} from "../Resource.ts";
 
+// I had to turn this off what might be dangerous. This is only needed for a single use case.
+// In the Admin Panel when creating a resource and then adding a cost there is a pretty specific issue.
+// The resource is yet being created so it can not be found in the state.
+// But when adding a cost the resource itself is set as the gain value. In there it should display its own icon
+// To do so I currently need the useResource.get(gain.key) to find the right image.
+// But when I do the resource is not yet being added to the state so it won't be found.
+// ---
+// [TL:DR] This warning needs to be turned off unless I found a better solution to pass images of resource costs
+
 export const get = <K extends string, T extends string>(key: K, state: ResourceState<K, T>[]): Resource<K, T> => {
   const _this = state.find(resource => resource.key === key);
-  if (!_this) throw new Error("Could not find resource key " + key);
-  return new Resource<K, T>(_this)
+  // if (!_this) throw new Error("Could not find resource key " + key);
+  return new Resource<K, T>(_this!)
 }
--- src/Resource/useResourceClone.ts
+++ src/Resource/useResourceClone.ts
@@ -138,7 +138,6 @@ export const useResourceClone = <K extends string, T extends string>(
     setKey,
     setMin,
     setMax,
-    // getResourceState,
   }
 }
 
