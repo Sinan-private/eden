4e367a6d6b09c69e1764053bfdee8bbcf1803c2b
Sinan Theuvsen <sinan.alk@gmail.com>
2025-01-10 22:55:29 +0100

    Just a test

--- src/Resource/ResourceHandler/specificTypes.ts
+++ src/Resource/ResourceHandler/specificTypes.ts
@@ -9,3 +9,8 @@ export type ResourceClass = Resource<ResourceKeys, ResourceTypes>;
 export type ResourceState = ResourceClass['state'];
 export type ResourceStoreClass = ResourceStore<ResourceKeys, ResourceTypes>;
 export type TradeChange = {key: ResourceKeys, value: number};
+// export type ResourceKeysWithLevels = `${ResourceKeys}_level_${1 | 2 | 3 | 4 | 5}`;
+// // Remove `_level_X` suffix if it exists, otherwise keep the key as-is
+// export type ResourceKeysWithoutLevels = ResourceKeys extends `${infer Base}_level_${number}`
+//   ? Base
+//   : ResourceKeys;
