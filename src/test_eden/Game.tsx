8049c17b3898a1ca6d6c6331a93f37fe93683392
Sinan Theuvsen <sinan.alk@gmail.com>
2025-04-20 11:26:26 +0200

    Mui is removed

--- src/test_eden/Game.tsx
+++ src/test_eden/Game.tsx
@@ -1,12 +1,10 @@
-import {Box} from "@mui/material";
-import styled from "styled-components";
+import {observer} from "mobx-react";
 import {Gaja} from "./Gaja/Gaja.tsx";
 import upstream_image from '../assets/images/upstream.gif';
 import {Background} from "./Background.tsx";
 import {DebuggingComponents} from "./Debugging/DebuggingComponents.tsx";
 import {Interface} from "./Interface/Interface.tsx";
 import {game} from "@/test_eden/Classes/Game/createSingletonGame.ts";
-import {observer} from "mobx-react";
 import {GameClass} from "@/test_eden/Classes/Game/GameClass.ts";
 
 const IMAGE_HEIGHT = 400
@@ -19,10 +17,8 @@ declare global {
 
 export const Game = () => {
   window.game = game();
-  // Test trade
-  // game.resources.trade([{key: 'dirty_mana_level_1', value: 1}], [{key: 'raw_mana_level_1', value: 1}], 0.2).tradeIfPossible()
   return (
-    <Screen id="Game" className="font-geist-sans text-sm antialiased">
+    <Screen id="Game">
       <Background/>
       <Gaja/>
       <Upstream/>
@@ -32,16 +28,11 @@ export const Game = () => {
   )
 }
 
-
-
-const Screen = styled('div')`
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 100vh;
-    overflow: hidden;
-`
+const Screen = ({children, id}: {children: React.ReactNode; id: string}) => (
+  <div id={id} className="fixed top-0 left-0 w-screen h-screen overflow-hidden font-geist-sans text-sm antialiased">
+    {children}
+  </div>
+)
 
 const Upstream = observer(() => {
   const {upstream, behemoth} = game();
@@ -49,43 +40,9 @@ const Upstream = observer(() => {
   return position < -300
     ? null
     : (
-      <UpstreamContainer $position={position}>
-        <Box sx={{
-          width: '100%',
-          height: 200,
-          position: 'relative',
-          left: 0,
-          zIndex: 1,
-          backgroundImage: `url(${upstream_image})`,
-          backgroundPosition: '0 280px',
-        }}/>
-        <Box sx={{
-          width: '100%',
-          height: 400,
-          background: 'black'
-        }}/>
-      </UpstreamContainer>
+      <div id="Upstream" className="fixed pointer-events-none w-full h-[600px] left-0 z-[100]" style={{bottom: -400 + position}}>
+        <div className="relative w-full h-[200px] left-0 z-1" style={{backgroundImage: `url(${upstream_image})`, backgroundPosition: '0 280px'}} />
+        <div className="w-full h-[400px] bg-black" />
+      </div>
     )
 })
-
-const UpstreamContainer = styled(Box).attrs<{ $position: number }>((props) => ({
-  style: {
-    bottom: -400 + props.$position,
-  }
-}))`
-    pointer-events: none;
-    width: 100%;
-    height: 600px;
-    position: fixed;
-    left: 0;
-    z-index: 100;
-`
-
-
-// const HoverConditionally = styled(Box)<{ $disabled: boolean }>`
-//     ${({ $disabled }) => !$disabled && `
-//     &:hover {
-//       background: #202927;
-//     }
-//   `}
-// `
